ba91cc7dfbcd6ddb0ff309a52b3f58a355f19159
Nathan Nieuwenhuizen <nathan-san@live.nl>
2019-05-19 18:59:49 +0200

    small fix

--- assets/php/createPart.php
+++ assets/php/createPart.php
@@ -1,13 +1,13 @@
 <?php
 
-$servername = "remotemysql.com";
-$username = "NBEf0XGIkY";
-$password = "5PpbZUQgNX";
-$dbname = "NBEf0XGIkY";
 // $servername = "remotemysql.com";
-// $username = "mTTXio0REs";
-// $password = "43Kz77Z0Ql";
-// $dbname = "mTTXio0REs";
+// $username = "NBEf0XGIkY";
+// $password = "5PpbZUQgNX";
+// $dbname = "NBEf0XGIkY";
+$servername = "remotemysql.com";
+$username = "mTTXio0REs";
+$password = "43Kz77Z0Ql";
+$dbname = "mTTXio0REs";
 
 
 // Create connection
