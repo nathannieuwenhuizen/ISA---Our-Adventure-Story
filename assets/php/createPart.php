e59d7e49c7ba5e55cfd2634f96bff2f20e8d3377
Nathan Nieuwenhuizen <nathan-san@live.nl>
2019-07-16 17:19:25 +0200

    duplicate entry bug fix

--- assets/php/createPart.php
+++ assets/php/createPart.php
@@ -22,46 +22,60 @@ if ($parentEnd == "1") {
     //header("location: ../../?storypart=". $parentID);
     return;
 }
-$sql = "INSERT INTO `storyparts` (`ID`, `start`, `end`, `option_text`, `content_text`, `question_text`, `option_IDs`, `Date`, `layer`, `image`, `parentID`, `storyID`)" .
+
+//this prevents duplicate entries
+$sql = "SELECT * FROM storyparts WHERE (parentID = $parentID AND content_text = '$consequence' AND option_text = '$option')  LIMIT 1";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // echo "<br>A part already exist.<br>";
+    header("location: ../../story.php?storypart=". $parentID);
+} else {
+    // echo "Error: " . $sql . "<br>" . $conn->error . "<br> pee pee!";
+    
+    $sql = "INSERT INTO `storyparts` (`ID`, `start`, `end`, `option_text`, `content_text`, `question_text`, `option_IDs`, `Date`, `layer`, `image`, `parentID`, `storyID`)" .
 " VALUES (NULL, '0', '$end', '$option', '$consequence', '$question', '', NOW(), $layer, '$image', $parentID, $storyID);";
 
-$last_id = -1;
+    $last_id = -1;
 
 
-if ($conn->query($sql) === TRUE) {
-    $last_id = $conn->insert_id;
-    //echo "<br>New part created successfully.<br>";
-} else {
-    //echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
- }
-
-
-
-//echo "<br><br><br>";
-/*
-Updates the option list of the parent ID
-*/
-$parentOptions = htmlspecialchars($_POST['parentOptions']);
-if ($last_id != -1) {
-    if ( $parentOptions != "") {
-        $parentOptions .= ", ";
+    if ($conn->query($sql) === TRUE) {
+        $last_id = $conn->insert_id;
+        //echo "<br>New part created successfully.<br>";
+    } else {
+        //echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
     }
-    $parentOptions .= $last_id;
-}
 
-// echo $sql;
-$sql = "UPDATE `storyparts` SET `option_IDs` = '". $parentOptions . "' WHERE `storyparts`.`ID` = ". $parentID . ";";
-if ($conn->query($sql) === TRUE) {
-    //echo "<br>Optionlist is updated inserted ID is: " . $last_id . "<br>";
- } else {
-     //echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
+
+
+    //echo "<br><br><br>";
+    /*
+    Updates the option list of the parent ID
+    */
+    $parentOptions = htmlspecialchars($_POST['parentOptions']);
+    if ($last_id != -1) {
+        if ( $parentOptions != "") {
+            $parentOptions .= ", ";
+        }
+        $parentOptions .= $last_id;
+    }
+
+    // echo $sql;
+    $sql = "UPDATE `storyparts` SET `option_IDs` = '". $parentOptions . "' WHERE `storyparts`.`ID` = ". $parentID . ";";
+    if ($conn->query($sql) === TRUE) {
+        //echo "<br>Optionlist is updated inserted ID is: " . $last_id . "<br>";
+    } else {
+        //echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
+    }
+
+
+    //backup
+    //echo "<br><a href='../../?storypart=". $last_id."'>go back to page</a>";
+    /*
+    Redirect ot the new page
+    */
+    header("location: ../../story.php?storypart=". $last_id);
+    die();
  }
 
-
-//backup
-//echo "<br><a href='../../?storypart=". $last_id."'>go back to page</a>";
-/*
-Redirect ot the new page
-*/
-header("location: ../../story.php?storypart=". $last_id);
 ?>
